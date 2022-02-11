ea090664371d61a738ceed5903349f6f8935b909
Aleksandra Ljubisavljevic <lj.aleksandra@gmail.com>
2022-02-11 19:59:23 +0100

    Izmena komponente ProductItem

--- client/src/components/ProductItem.tsx
+++ client/src/components/ProductItem.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { RouteComponentProps, withRouter } from 'react-router'
+//import { RouteComponentProps, withRouter } from 'react-router'
 import { Button, Card, Image } from 'semantic-ui-react'
 import { Product } from '../model'
+import {useParams} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
     product: Product,
     addOrder: () => void
 }
-export default withRouter(function ProductItem(props: Props & RouteComponentProps) {
+export default (function ProductItem(props: Props) {
+    const params = useParams();
+    let navigate = useNavigate();
     return (
         <Card centered link >
             <Image className='short' onClick={() => {
-                props.history.push('/products/' + props.product.id)
+                navigate('/products/' + props.product.id)
             }} src={`data:image/jpeg;base64,${props.product.picture}`} wrapped ui={false} />
             <Card.Content onClick={() => {
-                props.history.push('/products/' + props.product.id)
+                navigate('/products/' + props.product.id)
             }} >
                 <Card.Header>{props.product.name}</Card.Header>
                 <Card.Description >{props.product.price}RSD</Card.Description >
